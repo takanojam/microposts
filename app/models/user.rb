9aa6eb2330d1286b9b8104508b8b9ee90fe09431
yoshiyukitakano <takanojam@gmail.com>
2016-05-08 13:57:31 +0000

    location model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
-  before_save { self.email = self.email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+  before_action { self.email = self.email.downcase }
+  validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  has_secure_password
+  validates :email, presence: true, length: { maximum: 225},
+  format: { with: VALID_EMAIL_REGEX },
+  uniqueness: { case_sensitive: false }
+  validates :location, absence: true,
+                       on: :create
+  validates :location, allow_blank: true,
+                       length: { minimum: 2, maximum: 20 },
+                       on: :update
+    has_secure_password
 end
