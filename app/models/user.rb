2a8c911150bddd795ad9b541a1ee66213874f757
yoshiyukitakano <takanojam@gmail.com>
2016-05-22 07:03:48 +0000

    secondtime

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
   has_many :follower_users, through: :follower_relationships, source: :follower
 
   # 他のユーザーをフォローする
-  def follow(other_usera)
+  def follow(other_user)
     following_relationships.find_or_create_by(followed_id: other_user.id)
   end
 
